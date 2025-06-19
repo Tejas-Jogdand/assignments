aeaf56e62bc613aeeef46ea67a4ee9deef685f18
Tejas Jogdand <tejasjogdand37@gmail.com>
2025-06-19 18:07:59 +0530

    Spread operator use

---
+++ week-2/02-nodejs/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+const arr = [1,2,3,4,5];
+console.log(arr);
+console.log(...arr);
+console.log(Math.max(...arr));
+
+const str = "Tejas";
+console.log(str);
+console.log(...str);
+let strArr = [...str];
+console.log(strArr);
+console.log(Math.max(...str));

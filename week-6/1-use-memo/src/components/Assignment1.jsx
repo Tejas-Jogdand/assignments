2c048e4ab6ecbaf2253a1d5d4e7b6c9bdf724494
Tejas Jogdand <tejasjogdand37@gmail.com>
2025-07-03 23:53:02 +0530

    useMemo Assignments done

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
 export function Assignment1() {
     const [input, setInput] = useState(0);
+    const [count, setCount] = useState(0);
     // Your solution starts here
-    const expensiveValue = 0; 
-    // Your solution ends here
+    //3 => 3*2*1
+    const factorial =  useMemo(()=>{
+        console.log("inside renders")
+        let expensiveValue = 1;
+        for(let i = 1; i <= input; i++) {
+        expensiveValue = expensiveValue * i;
+        }
+        return expensiveValue
+    },[input])
 
-    return (
-        <div>
-            <input 
-                type="number" 
-                value={input} 
-                onChange={(e) => setInput(Number(e.target.value))} 
-            />
-            <p>Calculated Value: {expensiveValue}</p>
-        </div>
-    );
+
+// Your solution ends here
+
+return (
+    <div>
+        <input
+            type="number"
+            onChange={(e) => setInput(Number(e.target.value))}
+        />
+        <button onClick={()=>setCount(count+1)}>Click me</button>
+        <p>Calculated Value: {input == '' ? 0 : factorial}</p>
+    </div>
+);
 }
--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,20 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Banana', value: 30 }
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    console.log("Renders");
+    const totalValue = useMemo(()=>{
+        let sum = 0;
+        items.forEach(i => {
+        sum += i.value; 
+    })
+    return sum;
+    },[items])
+    
     // Your code ends here
     return (
         <div>
